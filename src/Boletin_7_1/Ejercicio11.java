1a1f2ab4625d9518e37f5e884fcad7897d4cbccd
roberto <robertocd509@gmail.com>
2024-04-22 12:46:15 +0200

    Cambios dia 18/04

--- src/Boletin_7_1/Ejercicio11.java
+++ src/Boletin_7_1/Ejercicio11.java
@@ -7,6 +7,6 @@ import java.util.regex.Pattern;
 public class Ejercicio11 {
     public static void main(String[] args) {
         Path fichero = Paths.get(".\\src\\Boletin_7_1\\Fichero11.txt");
-        Pattern patron = Pattern.compile("(\\p{Lu}{2,})\\s(\\p{Lu}{2,})\\s(\\p{Lu}{2,})\\s[^/\]");
+        Pattern patron = Pattern.compile("(\\p{Lu}{2,})\\s(\\p{Lu}{2,})\\s(\\p{Lu}{2,})\\s([^\\\\/:\"\\*\\?|<>])");
     }
 }
